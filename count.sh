d2abcbfeec28a45c828e2493c199878a3f7669d7
Jon Morehouse <morehousej09@gmail.com>
2012-08-11 02:33:57 -0700

    changed count.sh bash script to be universal. Allows for codeigniter specific or all files.

--- count.sh
+++ count.sh
@@ -1,26 +1,56 @@
 #!/bin/bash
-
-directory[0]="application/libraries/"
-directory[1]="application/controllers/"
-directory[2]="application/models/"
-directory[3]="application/views/"
-directory[4]="resources/javascript/local/"
-directory[5]="resources/javascript/live/"
-directory[6]="resources/css/live/"
-directory[7]="resources/css/local/"
-
-lines=0
 words=0
-for i in "${directory[@]}"
-do
-	for z in `find $i -name \*.*`
+lines=0
+codeigniter ()
+{
+	directory[0]="application/libraries/"
+	directory[1]="application/controllers/"
+	directory[2]="application/models/"
+	directory[3]="application/views/"
+	directory[4]="resources/javascript/local/"
+	directory[5]="resources/javascript/live/"
+	directory[6]="resources/css/live/"
+	directory[7]="resources/css/local/"
+
+	for i in "${directory[@]}"
 	do
-		file_lines=`wc -l < $z`
-		file_words=`wc -w < $z`
-		let "words += $file_words"
-		let "lines+= $file_lines"
+		for z in `find $i -name \*.*`
+		
+		do
+			if [[ -f ${z} ]]; then
+
+				file_lines=`wc -l < $z`
+				file_words=`wc -w < $z`
+				let "words+=$file_words"
+				let "lines+=$file_lines"
+				
+			fi
+		done
 	done
-done
+}
+
+all ()
+{
+	for file in $(find . -type f -name "*.*")
+		do
+			# check if file exists
+			if [[ -f ${file} ]]; then
+				file_lines=`wc -l < "${file}"` #get the lines for the current file
+				file_words=$(wc -w < "${file}") #get the words for the current file
+
+				let "words+=$file_words" #add the words
+				let "lines+=$file_lines" #add the lines
+			fi
+		done
+}
+
+
+if [[ ${#1} == 0 ]]; then
+	all
+else
+	codeigniter
+fi
+
 
 echo -e "Lines: $lines \nWords: $words"
 
