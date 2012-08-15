fa8f41c1d816405a215aa6dbb9b8d786a0b7c4df
Jon Morehouse <morehousej09@gmail.com>
2012-08-14 20:08:28 -0700

    Changed zip_current to allow for a flag to create temporary archive

--- zip_current.sh
+++ zip_current.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
 # this executable is primarily for working on production_test to archive my projects with one line. Archive.zip is for allowing for the ssh upload!
+archive=true
+if [[ ${#1} > 0 ]]; then
+	archive=false
+fi
 
 file_name=$(date +"%m_%d_%H_%I".zip); #month_day_hour_minute.zip
 
 permanent_archive="../archive/code_base/$file_name";
 temp_archive="Archive.zip";
 
-rm Archive.zip;
-zip -r $temp_archive *;
+# rm Archive.zip;
+if [[ archive ]]; then
+	zip -r $temp_archive *;
+fi
+
 zip -r $permanent_archive *; 
 
+if [[ archive ]]; then
+	echo -e "\n\tTemporary archive created."
+fi
+
+echo -e "\n\tPermanent archive created at {$permanent_archive}"
 
 
