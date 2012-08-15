eeabeb81ee9353ca57b4dfe84f253719930ce282
Jon Morehouse <morehousej09@gmail.com>
2012-08-14 20:12:34 -0700

    Fixed bug in zip_current

--- zip_current.sh
+++ zip_current.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # this executable is primarily for working on production_test to archive my projects with one line. Archive.zip is for allowing for the ssh upload!
-archive=true
+archive=false
 if [[ ${#1} > 0 ]]; then
-	archive=false
+	archive=true
 fi
 
 file_name=$(date +"%m_%d_%H_%I".zip); #month_day_hour_minute.zip
@@ -12,13 +12,15 @@ permanent_archive="../archive/code_base/$file_name";
 temp_archive="Archive.zip";
 
 # rm Archive.zip;
-if [[ archive ]]; then
+if ${archive:=true} ; then
+
 	zip -r $temp_archive *;
 fi
 
 zip -r $permanent_archive *; 
 
-if [[ archive ]]; then
+if ${archive:=true} ; then
+
 	echo -e "\n\tTemporary archive created."
 fi
 
