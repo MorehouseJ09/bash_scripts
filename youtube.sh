367c53d8378fe88ae97b3ec57d4349416458dd3a
MorehouseJ09 <morehousej09@gmail.com>
2012-08-29 16:03:03 -0700

    Added kill programs bash script

--- youtube.sh
+++ youtube.sh
@@ -2,23 +2,16 @@
 
 # this program will take in the video name, the artist and finally the new name for the song
 
-url=${1}
-name=${2}
 # default folder will be Greatest Hits Dub
-default_folder="${HOME}/Music/Greatest Hits-Dub/"
+default_folder="${HOME}/Music/House/"
 
 # make sure that parameters were given
 
-if [[ ${#1} == 0 ]]; then
 	echo "Please enter a url"
 	read url
-fi
-	
-if [[ ${#2} == 0 ]]; then
-	echo "Please enter a file name"
-	read name
-fi
 
+	echo "Please enter a filename"
+	read name
 
 # check that 3rd parameter is given
 if [[ ${#3} == 0 ]]; then
@@ -26,30 +19,31 @@ if [[ ${#3} == 0 ]]; then
 
 # 3rd parameter is given -- now check that it exists
 else
-	folder="${HOME}/Music/${3}"
+	folder="$HOME/Music/$3"
 
 	# check to make sure that the folder exists
 	if [ ! -d "${folder}" ]; then
-		folder="${default_folder}"
+		folder="$default_folder"
 	fi
 fi
 
 # check the folder has a trailing slash before adding the file name!
-
 if [ "`echo "${folder}" | tail -c 2`" != "/" ]; then
-	folder="${folder}/"
+	folder="$folder/"
 fi
 
-
-# check to make sure the file name is .mp3
-if [ "`echo "${name}" | tail -c 5`" != ".mp3" ]; then
-	name="${name}.mp3"
+if [[ "${name#*.}" != "mp3" ]]; then
+	name="$name.mp3"
 fi
 
-echo "${name}"
+# We are going to store a "temp file in the ~/.music downloads"
+cd ~/.music_downloads
+name="$folder$name"
+temp_name="$(ls | wc -l).mp3"
+temp_name="test.mp3"
 
-# final file name
-file_name="${folder}${name}"
+youtube-dl --extract-audio --audio-format mp3 -o test.mp3  "${url}"
 
-youtube-dl --extract-audio --audio-format mp3 -o "${file_name}" $url
-open -a iTunes "${file_name}"
+# cp $temp_name "${name}"
+# open -a itunes $file_name
+
